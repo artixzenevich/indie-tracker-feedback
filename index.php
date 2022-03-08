9e9251acf06ecfe148958fbdedfb863e0af8bbc4
Artik Zenevich <azenevich91@gmail.com>
2022-03-08 12:12:41 +0300

    init

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'vendor/autoload.php';
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$text = $_POST['mail-text'];
+
+$title = $subject;
+$body = "
+<h2>$title</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br><br>
+<b>Сообщение:</b><br>$text
+";
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = $_ENV['USERNAME']; // Логин на почте
+    $mail->Password   = $_ENV['PASSWORD']; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom($_ENV['EMAIL'], $name); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress($_ENV['EMAIL']);  
+ 
+
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
